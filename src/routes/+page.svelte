a0089e2f3dfaf9ee4d7801f8ec0c31c0a169c269
Vonn Adrian C. Jutar <vonnadrianjutar123@gmail.com>
2024-08-06 22:41:22 +0800

    signin

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -185,8 +185,7 @@
 			fill="none"
 			viewBox="0 0 24 24"
 			stroke-width="1.5"
-			stroke="currentColor"
-			class="w-[4vw] h-[4vw] max-w-[60px] max-h-[60px] min-w-[12px] min-h-[12px] mr-auto mt-2"
+			class="w-[4vw] h-[4vw] max-w-[60px] max-h-[60px] min-w-[12px] min-h-[12px] mr-auto mt-2 stroke-blue-900 hover:stroke-blue-800 transition-colors duration-200 ease-in-out"
 			on:click={handleClick}
 			on:keydown={handleKeyDown}
 			tabindex="0"
@@ -199,6 +198,7 @@
 				d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
 			/>
 		</svg>
+
 		<LoginModal />
 	</div>
 
